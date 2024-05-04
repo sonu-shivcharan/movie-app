a3db41d542c30ee1c9f8cdc26bb6a6524242ebc4
sonu-shivcharan <sonushivcharan@outlook.com>
2024-05-04 16:40:18 +0530

    "added sort and search functionality'

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,57 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Movies from './components/Movies';
-import Banner from './components/Banner';
-import WatchList from './components/WatchList';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Movies from "./components/Movies";
+import Banner from "./components/Banner";
+import WatchList from "./components/WatchList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
   const [movies, setMovies] = useState([]);
   const [watchlist, setWatchlist] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     let itemsFromLocalStore = localStorage.getItem("movieApp");
-    if(!itemsFromLocalStore){
+    if (!itemsFromLocalStore) {
       return;
     }
     setWatchlist(JSON.parse(itemsFromLocalStore));
-  }, [])
-  function addToWatchlist(movieObj){
-    let newWatchlist =[...watchlist, movieObj]
+  }, []);
+  function addToWatchlist(movieObj) {
+    let newWatchlist = [...watchlist, movieObj];
     setWatchlist(newWatchlist);
     localStorage.setItem("movieApp", JSON.stringify(newWatchlist));
     console.log(newWatchlist);
   }
-  function removeFromWatchlist(movieObj){
-    let newWatchlist = watchlist.filter((movie)=>movie.id!=movieObj.id);
+  function removeFromWatchlist(movieObj) {
+    let newWatchlist = watchlist.filter((movie) => movie.id != movieObj.id);
     setWatchlist(newWatchlist);
   }
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<> <Banner/> <Movies setMovies={setMovies} movies={movies} addToWatchlist={addToWatchlist} removeFromWatchlist={removeFromWatchlist} watchlist={watchlist}/></>} />
-        <Route path="/watchlist" element={<WatchList watchlist={watchlist} />} />
+        <Route
+          path="/"
+          element={
+            <>
+              {" "}
+              <Banner />{" "}
+              <Movies
+                setMovies={setMovies}
+                movies={movies}
+                addToWatchlist={addToWatchlist}
+                removeFromWatchlist={removeFromWatchlist}
+                watchlist={watchlist}
+              />
+            </>
+          }
+        />
+        <Route
+          path="/watchlist"
+          element={
+            <WatchList watchlist={watchlist} setWatchlist={setWatchlist} />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function WatchList({watchlist}) {
+function WatchList({ watchlist, setWatchlist }) {
+  const [searchVal, setSearchInput] = useState("");
+  
+  function sortDescending(){
+    let sorted = watchlist.sort((a, b)=>b.vote_average-a.vote_average);
+    setWatchlist([...sorted]);
+  }
+  function sortAscending(){
+    let sorted = watchlist.sort((a, b)=>a.vote_average-b.vote_average);
+    setWatchlist([...sorted]);
+  }
+  function handleSearch(e){
+    setSearchInput(e.target.value);
+  }
   return (
     <div>
       <div className="flex justify-center">
@@ -8,36 +21,45 @@ function WatchList({watchlist}) {
           type="text"
           placeholder="Search for movies"
           className="bg-gray-100 my-4 p-3 max-w-[350px] w-full rounded-xl outline-none"
+          value={searchVal}
+          onChange={handleSearch} 
         />
       </div>
       <div className="p-4">
         <table className="w-full">
-          <thead className="bg-blue-400/30 text-blue-900 p-4 border-b-[3px] ">
-            <tr className="py-[1rem]">
+          <thead className="">
+            <tr className="bg-blue-100 p-4 h-[3rem]">
               <th>Name</th>
-              <th>Rating</th>
+              <th>
+                <div className="flex items-center justify-centers">
+                <div onClick={sortAscending} className="p-2"><i className="fa-solid fa-arrow-down"></i></div>
+                <div className="px-2">Rating</div>
+                <div onClick={sortDescending} className="p-2"><i className="fa-solid fa-arrow-up"></i></div>
+                </div>
+                </th>
               <th>Popularity</th>
               <th>Genre</th>
             </tr>
           </thead>
           <tbody>
-            {
-              watchlist.map((movie)=>{
-                return (
+            {watchlist.filter((movie)=>{
+              return movie.title.toLowerCase().includes(searchVal.toLocaleLowerCase())
+            }).map((movie) => {
+              return (
                 <tr className="text-center border-b-[3px]" key={movie.id}>
-              <td className="flex items-center ">
-                <img className="w-[150px] p-2" src={`https://images.tmdb.org/t/p/original/${movie.poster_path}`}></img>
-                <div className="mx-4 font-bold">{movie.title}</div>
-              </td>
-              <td>{movie.vote_average.toFixed(2)}</td>
-              <td>{movie.popularity}</td>
-              <td>Action</td>
-            </tr>
-                )
-              })
-
-            }
-            
+                  <td className="flex items-center ">
+                    <img
+                      className="w-[150px] p-2"
+                      src={`https://images.tmdb.org/t/p/original/${movie.poster_path}`}
+                    ></img>
+                    <div className="mx-4 font-bold">{movie.title}</div>
+                  </td>
+                  <td>{movie.vote_average.toFixed(2)}</td>
+                  <td>{movie.popularity}</td>
+                  <td>Action</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
